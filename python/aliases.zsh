3d66acec97ab215df32531658ca8e62df427b3cc
FinalAngel <angelo.dini@me.com>
2020-06-02 17:11:51 +0200

    use neovim

---
+++ python/aliases.zsh
@@ -0,0 +1,1 @@
+alias blubb=ls
---
+++ vim/aliases.zsh
@@ -0,0 +1,8 @@
+# use nvim as default editor
+export SVN_EDITOR="nvim"
+export EDITOR="nvim"
+export LANG="de_CH.UTF-8"
+export LC_ALL="de_CH.UTF-8"
+
+# use nvim as vim standard
+alias vim=nvim
