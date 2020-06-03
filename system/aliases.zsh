04481330cbd93f24b649af27020bc3d9bec47c9c
FinalAngel <angelo.dini@me.com>
2020-06-03 08:45:00 +0200

    setup aliases and paths

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,88 +1,71 @@
 #!/bin/zsh
-alias reload!=". ~/.zshrc"
 
-# add alias to start and adapt dotfiles
-alias dot="cd ~/.dotfiles && code ."
+# detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+	export LS_COLORS='no=00:fi=00:di=01;31:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+else # macOS `ls`
+	colorflag="-G"
+	export LSCOLORS='BxBxhxDxfxhxhxhxhxcxcx'
+fi
 
-# =====================================================
-# MY OLD ALIASES
+# utilities
+alias ..="cd .."
+alias ...="cd ../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
 
-# PATH
-# export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH
-export PATH=/usr/local/bin:/usr/local/sbin:~/bin:$PATH
-export PYTHONPATH=$PYTHONPATH
-export MANPATH="/usr/local/man:$MANPATH"
-
-# Virtual Environment
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Sites
-export PIP_REQUIRE_VIRTUALENV=true
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2
-source /usr/local/bin/virtualenvwrapper.sh
-
-# Owner
-export USER_NAME="Angelo Dini"
-# eval "$(pyenv init -)"
-
-# FileSearch
-function f() { find . -iname "*$1*" ${@:2} }
-function r() { grep "$1" ${@:2} -R . }
-
-#mkdir and cd
-function mkcd() { mkdir -p "$@" && cd "$_"; }
-
-# Custom
-alias ls="ls -FG"
-alias ip="ifconfig | grep netmask | grep -v 127.0.0.1"
-alias ws="cd ~/Sites/"
-alias wsc="cd ~/Sites/_cloud/"
-alias wsd="cd ~/Sites/_projects/"
-alias venv="source env/bin/activate"
-alias denv="deactivate"
-alias coreshock="ssh finalangel@10.0.1.29"
-alias python="python3"
-alias pip="pip3"
-
-# python
-# export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
-# export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-# export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
-# export PKG_CONFIG_PATH="/usr/local/opt/openssl@1.1/lib/pkgconfig"
-
-# productivity
-alias rock="cd ~/Library/Application\ Support/Steam/steamapps/common/Rocksmith2014/dlc && open ."
-alias wipe="find . | grep -E '(__pycache__|\.pyc|\.pyo|\.DS_STORE|\.eggs|\.egg-info$)' | xargs rm -rf"
-alias lancet="/Users/finalangel/.virtualenvs/lancet/bin/lancet"
-alias system="sudo htop"
-alias youtube="youtube-dl -f bestvideo+bestaudio"
-alias cat="ccat"
+alias c="clear"
+alias l="ls -lF ${colorflag}"
+alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-directories-first"
+alias la="ls -lAF ${colorflag}"
+alias ls="command ls ${colorflag}"
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+alias path='echo -e ${PATH//:/\\n}'
+alias reload="source $HOME/.zshrc"
 alias top="htop"
+
+# directories
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias dot="dotfiles && code ."
+alias dotfiles="cd $HOME/.dotfiles"
+alias lib="cd $HOME/Library"
+alias ws="cd $HOME/Sites"
+alias wsc="cd $HOME/Sites/_cloud"
+alias wsd="cd $HOME/Sites/_projects"
+
+# development
+alias dc="docker-compose"
+alias dcrun="dc run --rm"
+alias dclog="dc logs -f"
+alias dclint="docker run --env-file=.lint -it -v $(pwd):/app divio/lint /bin/lint"
+alias killnode="rm -rf node_modules/ package-lock.json && npm install"
+# alias lancet="$HOME/.virtualenvs/lancet/bin/lancet" # enable for development
+alias pip="pip3"
+alias python="python3"
+
+# helper
+alias cleanup="find . | grep -E '(\.DS_STORE|__pycache__|\.pyc|\.pyo|\.eggs|\.egg-info$)' | xargs rm -rf"
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias flush="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias ip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
+alias week='date +%V'
+
+# os specific
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+alias killchrome="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+# fun stuff
+alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
+
+# additional aliases for git in git/gitconfig.symlink
 alias gt="gittower ."
 
-# docker
-alias dc="docker-compose"
-alias dcrunserver="dc run --rm web python manage.py runserver 0.0.0.0:80"
-alias dcpdb="dc run --rm --service-ports web"
-alias dcrun="dc run --rm web"
-alias dclog="dc logs -f"
-
-function py() { python2 $1 }
-# function dc() { docker-compose $1 }
-function server () {
-    local port="${1:-8000}"
-    sleep 1 && open "http://localhost:${port}/" &
-    python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
-}
-
-alias dclint="docker run --env-file=.lint -it -v $(pwd):/app divio/lint /bin/lint"
-
-# custom Vadim shit
-backup_cp() {
-    local now=$(date +'%Y-%m-%d.%H.%M')
-    local version=$(cat src/version.py | sed 's/.*"\(.*\)".*/\1/')
-    docker exec control-panel_postgres_1 pg_dump postgres://control:cQyDbLGT3HrPmFqdY4hlVSitKA0ExXUg@postgres:5432/control --file /app/${now}.dump
-    zip ${version}.${now}.zip -r data ${now}.dump
-    mv ${version}.${now}.zip ~/Desktop
-    rm ${now}.dump
-}
+# load function
+source "$HOME/.dotfiles/system/functions.zsh"
---
+++ system/functions.zsh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+
+# shorthand for file search
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+# dreate a new directory and enter it
+function mkd() { mkdir -p "$@" && cd "$_"; }
+
+# Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
+function targz() {
+  local tmpFile="${@%/}.tar";
+  tar -cvf "${tmpFile}" --exclude=".DS_Store" "${@}" || return 1;
+
+  size=$(
+    stat -f"%z" "${tmpFile}" 2> /dev/null; # macOS `stat`
+    stat -c"%s" "${tmpFile}" 2> /dev/null;  # GNU `stat`
+  );
+
+  local cmd="";
+  if (( size < 52428800 )) && hash zopfli 2> /dev/null; then
+    # the .tar file is smaller than 50 MB and Zopfli is available; use it
+    cmd="zopfli";
+  else
+    if hash pigz 2> /dev/null; then
+      cmd="pigz";
+    else
+      cmd="gzip";
+    fi;
+  fi;
+
+  echo "Compressing .tar ($((size / 1000)) kB) using \`${cmd}\`…";
+  "${cmd}" -v "${tmpFile}" || return 1;
+  [ -f "${tmpFile}" ] && rm "${tmpFile}";
+
+  zippedSize=$(
+    stat -f"%z" "${tmpFile}.gz" 2> /dev/null; # macOS `stat`
+    stat -c"%s" "${tmpFile}.gz" 2> /dev/null; # GNU `stat`
+  );
+
+  echo "${tmpFile}.gz ($((zippedSize / 1000)) kB) created successfully.";
+}
+
+# Determine size of a file or total size of a directory
+function fs() {
+  if du -b /dev/null > /dev/null 2>&1; then
+    local arg=-sbh;
+  else
+    local arg=-sh;
+  fi
+  if [[ -n "$@" ]]; then
+    du $arg -- "$@";
+  else
+    du $arg .[^.]* ./*;
+  fi;
+}
+
+# Use Git’s colored diff when available
+hash git &>/dev/null;
+if [ $? -eq 0 ]; then
+  function diff() {
+    git diff --no-index --color-words "$@";
+  }
+fi;
+
+# Create a data URL from a file
+function dataurl() {
+  local mimeType=$(file -b --mime-type "$1");
+  if [[ $mimeType == text/* ]]; then
+    mimeType="${mimeType};charset=utf-8";
+  fi
+  echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
+}
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+  local port="${1:-8000}";
+  sleep 1 && open "http://localhost:${port}/" &
+  # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+  # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+  python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port";
+}
+
+# Compare original and gzipped file size
+function gz() {
+  local origsize=$(wc -c < "$1");
+  local gzipsize=$(gzip -c "$1" | wc -c);
+  local ratio=$(echo "$gzipsize * 100 / $origsize" | bc -l);
+  printf "orig: %d bytes\n" "$origsize";
+  printf "gzip: %d bytes (%2.2f%%)\n" "$gzipsize" "$ratio";
+}
+
+# Run `dig` and display the most useful info
+function digga() {
+  dig +nocmd "$1" any +multiline +noall +answer;
+}
+
+# Normalize `open` across Linux, macOS, and Windows.
+# This is needed to make the `o` function (see below) cross-platform.
+if [ ! $(uname -s) = 'Darwin' ]; then
+  if grep -q Microsoft /proc/version; then
+    # Ubuntu on Windows using the Linux subsystem
+    alias open='explorer.exe';
+  else
+    alias open='xdg-open';
+  fi
+fi
+
+# `o` with no arguments opens the current directory, otherwise opens the given
+# location
+function o() {
+  if [ $# -eq 0 ]; then
+    open .;
+  else
+    open "$@";
+  fi;
+}
+
+# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
+# the `.git` directory, listing directories first. The output gets piped into
+# `less` with options to preserve color and line numbers, unless the output is
+# small enough for one screen.
+function tre() {
+  tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+}
+
+# backsup control panel and creates nice zip file
+backup_cp() {
+    local now=$(date +'%Y-%m-%d.%H.%M')
+    local version=$(cat src/version.py | sed 's/.*"\(.*\)".*/\1/')
+    docker exec control-panel_postgres_1 pg_dump postgres://control:cQyDbLGT3HrPmFqdY4hlVSitKA0ExXUg@postgres:5432/control --file /app/${now}.dump
+    zip ${version}.${now}.zip -r data ${now}.dump
+    mv ${version}.${now}.zip ~/Desktop
+    rm ${now}.dump
+}
