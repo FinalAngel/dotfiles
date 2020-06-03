9711dafbec3764205a51ea803c36ea265cd2c3fd
FinalAngel <angelo.dini@me.com>
2020-06-03 08:45:27 +0200

    additional install mechanisms

--- node/paths.zsh
+++ node/paths.zsh
@@ -1,2 +1,11 @@
+#!/bin/zsh
+
+# load nvm
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# load Node global installed binaries
+export PATH="$HOME/.node/bin:$PATH"
+
+# use project specific binaries before global ones
+export PATH="node_modules/.bin:vendor/bin:$PATH"
