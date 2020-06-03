bb6a95eb7545adfa2e5c2adf205f683097c7e4b9
FinalAngel <angelo.dini@divio.ch>
2020-06-03 16:22:53 +0200

    gpg alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -46,6 +46,7 @@ alias python="python3"
 # helper
 alias cleanup="find . | grep -E '(\.DS_STORE|__pycache__|\.pyc|\.pyo|\.eggs|\.egg-info$)' | xargs rm -rf"
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+alias listgpg="gpg --list-secret-keys --keyid-format LONG"
 alias flush="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
