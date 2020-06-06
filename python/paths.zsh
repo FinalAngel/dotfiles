a358f9dd2e5d3213bb37b6ca6f3fac10d9c77431
Angelo Dini <angelo.dini@divio.ch>
2020-06-06 22:39:52 +0200

    fix installation issues on fresh system

--- python/paths.zsh
+++ python/paths.zsh
@@ -6,7 +6,7 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Sites
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 export PIP_REQUIRE_VIRTUALENV=true # to avoid global installs
-export VIRTUALENVWRAPPER_PYTHON=python # uses alias
+export VIRTUALENVWRAPPER_PYTHON=python3
 
 # load virtualenvwrapper
 source /usr/local/bin/virtualenvwrapper.sh
