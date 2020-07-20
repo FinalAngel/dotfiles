5f9b86ce16c16e85bd89ec3aaff16c8806008a73
Angelo Dini <angelo.dini@divio.ch>
2020-07-20 17:14:04 +0200

    remove not working alias

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,5 +1,3 @@
 #!/bin/zsh
-alias pip="pip3"
 alias nukedb="docker-compose run --rm web dropdb -h postgres -U postgres db && docker-compose run --rm web createdb -h postgres -U postgres db"
-
 # alias lancet="$HOME/.virtualenvs/lancet/bin/lancet" # enable for development
