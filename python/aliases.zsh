d9d5b426106275357117eaec6408ad3807c3cc64
Angelo Dini <angelo.dini@divio.ch>
2020-08-06 16:39:08 +0200

    added cover alias

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 alias nukedb="docker-compose run --rm web dropdb -h postgres -U postgres db && docker-compose run --rm web createdb -h postgres -U postgres db"
 # alias lancet="$HOME/.virtualenvs/lancet/bin/lancet" # enable for development
+alias cover="coverage erase && coverage run setup.py test && coverage html"
